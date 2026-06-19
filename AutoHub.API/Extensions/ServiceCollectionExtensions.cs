e1ac2a7a8b38386c193ae1ac67c9fbbe19bc25f6
Jindal Mistry <mistryjindal@gmail.com>
2026-06-19 09:01:18 +0530

    Added Dependency Injection

---
+++ AutoHub.API/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,30 @@
+﻿using AutoHub.Application.Configurations;
+using AutoHub.Application.Interfaces;
+using AutoHub.Infrastructure.Configuration;
+using AutoHub.Infrastructure.Services;
+using AutoHub.Infrastructure.Services.Authentication;
+
+namespace AutoHub.API.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddApplicationServices (this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped<IPasswordHasher, PasswordHasher>();
+            services.AddScoped<IAuthService, AuthService>();
+            services.AddScoped<IJwtTokenGenerator, JwtTokenGenerator>();
+            services.AddScoped<IVehicleService, VehicleService>();
+            services.AddScoped<IDealerService, DealerService>();
+            services.AddScoped<IStorageService, StorageService>();
+            services.AddScoped<IVehicleImageService, VehicleImageService>();
+            services.AddScoped<IReservationService, ReservationService>();
+            services.AddScoped<IBackgroundJobService, BackgroundJobService>();
+            services.AddScoped<ICacheService, CacheService>();
+
+            services.Configure<JwtSettings>(configuration.GetSection("Jwt"));
+            services.Configure<MinioSettings>(configuration.GetSection("Minio"));
+
+            return services;
+        }
+    }
+}
