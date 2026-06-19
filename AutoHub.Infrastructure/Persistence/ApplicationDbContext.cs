3d6634f28d32480b96846aebbb03181266347949
Jindal Mistry <mistryjindal@gmail.com>
2026-06-19 09:00:04 +0530

    Added ApplicatinDbContext for Postgres connection

---
+++ AutoHub.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using AutoHub.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AutoHub.Infrastructure.Persistance;
+
+public class ApplicationDbcontext : DbContext
+{
+    public ApplicationDbcontext(DbContextOptions<ApplicationDbcontext> options) : base(options)
+    {
+    }
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<Dealer> Dealers { get; set; }
+    public DbSet<Vehicle> Vehicles { get; set; }
+    public DbSet<Favourite> Favourites { get; set; }
+    public DbSet<Inquiry> Inquiries { get; set; }
+    public DbSet<VehicleImage> VehicleImages { get; set; }
+    public DbSet<VehicleAnalytics> Analytics { get; set; }
+    public DbSet<Tag> Tags { get; set; }
+    public DbSet<VehicleTag> VehicleTags { get; set; }
+    public DbSet<Reservation> Reservations { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbcontext).Assembly);
+        base.OnModelCreating(modelBuilder);
+    }
+}
