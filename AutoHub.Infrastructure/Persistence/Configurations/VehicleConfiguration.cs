4755a8cc4aa50cd8bc515fad1f46ac2f2962df59
Jindal Mistry <mistryjindal@gmail.com>
2026-06-22 10:36:37 +0530

    Vehicle configuration EF added

---
+++ AutoHub.Infrastructure/Persistence/Configurations/VehicleConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using AutoHub.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AutoHub.Infrastructure.Persistence.Configurations;
+
+public class VehicleConfiguration : IEntityTypeConfiguration<Vehicle>
+{
+    public void Configure(EntityTypeBuilder<Vehicle> builder)
+    {
+        builder.HasIndex(x => x.Make);
+
+        builder.HasIndex(x => x.Model);
+
+        builder.HasIndex(x => x.Status);
+
+        builder.HasIndex(x => x.Price);
+
+        builder.HasIndex(x => x.Year);
+
+        builder.HasIndex(x => x.DealerId);
+
+        builder.HasIndex(x => x.FuelType);
+
+        builder.HasIndex(x => x.Transmission);
+
+        builder.HasIndex(x => x.CreatedAt);
+
+        builder.Property(x => x.Price)
+            .HasPrecision(18, 2);
+
+        builder.HasIndex(x => x.RegNo)
+            .IsUnique();
+    }
+}
