c7398eb21750fe3e84203f552fc249bdb266777e
Jindal Mistry <mistryjindal@gmail.com>
2026-06-22 10:36:07 +0530

    Vehicle service interface added

---
+++ AutoHub.Application/Interfaces/IVehicleService.cs
@@ -0,0 +1,24 @@
+﻿using AutoHub.Application.DTOs.Vehicles;
+
+namespace AutoHub.Application.Interfaces;
+
+public interface IVehicleService
+{
+    Task<VehicleResponse> CreateVehicleAsync(CreateVehicleRequest request, Guid userId);
+
+    Task<List<VehicleResponse>> GetMyVehiclesAsync(Guid userId);
+
+    Task<VehicleResponse> GetVehicleByIdAsync(Guid vehicleId);
+
+    Task<VehicleResponse> UpdateVehicleAsync(CreateVehicleRequest request, Guid userId, Guid vehicleId);
+
+    Task PublishVehicleAsync(Guid vehicleId, Guid userId);
+
+    Task DeleteVehicleAsync(Guid vehicleId, Guid userId);
+
+    Task UnpublishVehicleAsync(Guid vehicleId);
+
+    Task<PaginatedResponse<VehicleListingResponse>> SearchVehiclesAsync(VehicleSearchRequest request);
+
+    Task<VehicleFilterOptionsResponse> GetVehicleFilterOptionsAsync();
+}
