ac1ec8d9b31355bd349ea413a935d895920c8556
Jindal Mistry <mistryjindal@gmail.com>
2026-06-23 09:32:23 +0530

    Storage Config and Interface Implementation

---
+++ AutoHub.Infrastructure/Configuration/MinioSettings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoHub.Infrastructure.Configuration;
+
+public class MinioSettings
+{
+    public string Endpoint { get; set; } = string.Empty;
+
+    public string AccessKey { get; set; } = string.Empty;
+
+    public string SecretKey { get; set; } = string.Empty;
+
+    public string BucketName { get; set; } = string.Empty;
+
+    public bool UseSSL { get; set; }
+}
---
+++ AutoHub.Infrastructure/Services/StorageService.cs
@@ -0,0 +1,52 @@
+﻿using AutoHub.Application.Interfaces;
+using AutoHub.Infrastructure.Configuration;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+using Minio;
+using Minio.DataModel.Args;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoHub.Infrastructure.Services;
+
+public class StorageService : IStorageService
+{
+    private readonly IMinioClient _minioClient;
+
+    private readonly MinioSettings _settings;
+
+    public StorageService(IOptions<MinioSettings> settings)
+    {
+        _settings = settings.Value;
+
+        _minioClient = new MinioClient()
+            .WithEndpoint(_settings.Endpoint)
+            .WithCredentials(_settings.AccessKey, _settings.SecretKey)
+            .Build();
+    }
+
+    public async Task DeleteFileAsync(string objectName)
+    {
+        await _minioClient.RemoveObjectAsync(
+            new RemoveObjectArgs()
+            .WithBucket(_settings.BucketName)
+            .WithObject(objectName));
+    }
+
+    public async Task<string> UploadFileAsync(Stream stream, string fileName, string contentType, string folderName)
+    {
+        var objectName =
+        $"{folderName}/{Guid.NewGuid()}_{fileName}";
+
+        await _minioClient.PutObjectAsync(
+         new PutObjectArgs()
+            .WithBucket(_settings.BucketName)
+            .WithObject(objectName)
+            .WithStreamData(stream)
+            .WithObjectSize(stream.Length)
+            .WithContentType(contentType));
+
+        return objectName;
+    }
+}
