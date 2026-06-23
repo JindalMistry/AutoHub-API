c2a09700b64ea027de736179fd8e4078b4af0f56
Jindal Mistry <mistryjindal@gmail.com>
2026-06-23 09:31:45 +0530

    Storage Service interface

---
+++ AutoHub.Application/Interfaces/IStorageService.cs
@@ -0,0 +1,12 @@
+﻿namespace AutoHub.Application.Interfaces;
+
+public interface IStorageService
+{
+    Task<string> UploadFileAsync(
+        Stream stream,
+        string fileName,
+        string contentType,
+        string folderName);
+
+    Task DeleteFileAsync(string objectName);
+}
