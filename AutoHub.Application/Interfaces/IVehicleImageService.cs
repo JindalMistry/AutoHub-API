3d45ab915b77e7f8bf5a6203582cb4ef26496589
Jindal Mistry <mistryjindal@gmail.com>
2026-06-24 08:12:37 +0530

    Vehicle image interface

---
+++ AutoHub.Application/Interfaces/IVehicleImageService.cs
@@ -0,0 +1,15 @@
+﻿using AutoHub.Application.DTOs.Vehicles;
+
+namespace AutoHub.Application.Interfaces;
+
+public interface IVehicleImageService
+{
+    Task<List<VehicleImageResponse>> UploadImagesAsync(CreateVehicleImageRequest request);
+
+    Task<List<VehicleImageResponse>> GetImagesAsync(
+        Guid vehicleId);
+
+    Task DeleteImageAsync(
+        Guid imageId,
+        Guid userId);
+}
