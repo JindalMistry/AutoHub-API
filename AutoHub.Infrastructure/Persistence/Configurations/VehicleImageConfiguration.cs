56177f84a47a5f4d8b20d6fbc88ee76c43060563
Jindal Mistry <mistryjindal@gmail.com>
2026-06-24 08:12:09 +0530

    Vehicle image Config

---
+++ AutoHub.Infrastructure/Persistence/Configurations/VehicleImageConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using AutoHub.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoHub.Infrastructure.Persistence.Configurations;
+
+public class VehicleImageConfiguration : IEntityTypeConfiguration<VehicleImage>
+{
+    public void Configure(EntityTypeBuilder<VehicleImage> builder)
+    {
+        builder.HasIndex(o => o.VehicleId);
+
+        builder.HasKey(o => new
+        {
+            o.DisplayOrder,
+            o.VehicleId
+        });
+    }
+}
