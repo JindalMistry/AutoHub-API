b54ab852d3d574773bf692a69e32ac648608985f
Jindal Mistry <mistryjindal@gmail.com>
2026-06-25 10:42:26 +0530

    IFavouriteService and it's methods added

---
+++ AutoHub.Application/Interfaces/IFavouriteService.cs
@@ -0,0 +1,20 @@
+﻿using AutoHub.Application.DTOs.Vehicles;
+
+namespace AutoHub.Application.Interfaces;
+
+public interface IFavouriteService
+{
+    Task AddFavouriteAsync(
+        Guid vehicleId,
+        Guid userId);
+
+    Task RemoveFavouriteAsync(
+        Guid vehicleId,
+        Guid userId);
+
+    Task<PaginatedResponse<VehicleListingResponse>>
+        GetMyFavouritesAsync(
+            Guid userId,
+            int pageNumber,
+            int pageSize);
+}
