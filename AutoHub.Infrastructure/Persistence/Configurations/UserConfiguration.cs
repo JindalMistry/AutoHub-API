3b5d991bcd8696e18eab75d7a1931ab52e08b6a9
Jindal Mistry <mistryjindal@gmail.com>
2026-06-25 10:44:13 +0530

    change in User configuration

---
+++ AutoHub.Infrastructure/Persistence/Configurations/UserConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using AutoHub.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoHub.Infrastructure.Persistence.Configurations;
+
+public class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.HasIndex(o => o.IsActive);
+        builder.HasIndex(o => o.Role);
+        builder.HasIndex(o => o.Email)
+            .IsUnique();
+    }
+}
