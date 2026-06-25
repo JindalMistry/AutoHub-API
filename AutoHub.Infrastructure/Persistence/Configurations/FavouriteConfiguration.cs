9b1013d04211d21e6e96918203f489ae6a73d902
Jindal Mistry <mistryjindal@gmail.com>
2026-06-25 10:42:41 +0530

    Favourites table DB configuration added

---
+++ AutoHub.Infrastructure/Persistence/Configurations/FavouriteConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using AutoHub.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AutoHub.Infrastructure.Persistence.Configurations;
+
+public class FavouriteConfiguration : IEntityTypeConfiguration<Favourite>
+{
+    public void Configure(EntityTypeBuilder<Favourite> builder)
+    {
+        builder.HasKey(x => new
+        {
+            x.UserId,
+            x.VehicleId
+        });
+
+        builder.HasIndex(o => o.UserId);
+
+        builder.HasIndex(o => o.VehicleId);
+    }
+}
+
