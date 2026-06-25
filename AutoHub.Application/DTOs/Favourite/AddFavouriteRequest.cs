8c939ac2f4eb4f29dd92fe410b0dd1d217498594
Jindal Mistry <mistryjindal@gmail.com>
2026-06-25 10:42:10 +0530

    Favourites DTOs added

---
+++ AutoHub.Application/DTOs/Favourite/AddFavouriteRequest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoHub.Application.DTOs.Favourite;
+
+public class AddFavouriteRequest
+{
+    public Guid VehicleId { get; set; }
+}
---
+++ AutoHub.Application/DTOs/Favourite/RemoveFavouriteRequest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoHub.Application.DTOs.Favourite;
+
+public class RemoveFavouriteRequest
+{
+    public Guid VehicleId { get; set; }
+}
