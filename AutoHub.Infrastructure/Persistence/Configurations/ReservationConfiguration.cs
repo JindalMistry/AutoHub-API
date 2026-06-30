daa53b30ccc227c127b62ce18e65d3ba6aab0b9f
Jindal Mistry <mistryjindal@gmail.com>
2026-06-30 09:37:51 +0530

    Reservation entity configs added

---
+++ AutoHub.Infrastructure/Persistence/Configurations/ReservationConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using AutoHub.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AutoHub.Infrastructure.Persistence.Configurations;
+
+public class ReservationConfiguration : IEntityTypeConfiguration<Reservation>
+{
+    public void Configure(EntityTypeBuilder<Reservation> builder)
+    {
+        builder.HasIndex(x => x.VehicleId);
+
+        builder.HasIndex(x => x.UserId);
+
+        builder.HasIndex(x => x.Status);
+
+        builder.HasIndex(x => x.ExpiresAt);
+
+        builder.HasIndex(x => x.CreatedAt);
+
+        builder.HasIndex(o => o.VehicleId)
+                .IsUnique()
+                .HasFilter("\"Status\" = 0");
+    }
+}
