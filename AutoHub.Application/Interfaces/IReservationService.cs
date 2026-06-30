5dd4923d3f8d09240202fc801ac00b5b3cc2acf0
Jindal Mistry <mistryjindal@gmail.com>
2026-06-30 09:37:37 +0530

    Reservation abstraction added

---
+++ AutoHub.Application/Interfaces/IReservationService.cs
@@ -0,0 +1,33 @@
+﻿using AutoHub.Application.DTOs.Reservation;
+using AutoHub.Application.DTOs.Vehicles;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoHub.Application.Interfaces;
+
+public interface IReservationService
+{
+    Task<CreateReservationResponse> CreateReservationAsync(
+        CreateReservationRequest request,
+        Guid buyerId);
+
+    Task CancelReservationAsync(
+        Guid reservationId);
+
+    Task<List<ReservationResponse>>
+        GetMyReservationsAsync(
+            Guid buyerId,
+            ReservationStatus? status);
+
+    Task<ReservationResponse>
+        GetReservationByIdAsync(
+            Guid reservationId);
+
+    Task<PaginatedResponse<ReservationResponse>>
+        GetDealerReservationsAsync(
+            Guid userId,
+            ReservationStatus? status,
+            int pageNumber,
+            int pageSize);
+}
