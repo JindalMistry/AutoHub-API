e27ecfdc486d30581462cb3af0874d637f9a6bbe
Jindal Mistry <mistryjindal@gmail.com>
2026-06-30 09:37:22 +0530

    Reservation module DTOs added

---
+++ AutoHub.Application/DTOs/Reservation/CreateReservationRequest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoHub.Application.DTOs.Reservation;
+
+public class CreateReservationRequest
+{
+    public Guid VehicleId { get; set; }
+}
---
+++ AutoHub.Application/DTOs/Reservation/CreateReservationResponse.cs
@@ -0,0 +1,32 @@
+﻿using AutoHub.Application.DTOs.Vehicles;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoHub.Application.DTOs.Reservation;
+
+public class CreateReservationResponse
+{
+    public Guid Id { get; set; }
+}
+
+public class ReservationResponse
+{
+    public Guid Id { get; set; }
+
+    public Guid VehicleId { get; set; }
+
+    public required VehicleListingResponse Vehicle { get; set; }
+
+    public ReservationStatus Status { get; set; }
+
+    public DateTime ExpiresAt { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public string BuyerName { get; set; } = string.Empty;
+
+    public string BuyerEmail { get; set; } = string.Empty;
+
+    public string BuyerPhone { get; set; } = string.Empty;
+}
