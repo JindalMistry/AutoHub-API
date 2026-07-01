12c738acfd90e7eee56d21cb98078d61e50b551b
Jindal Mistry <mistryjindal@gmail.com>
2026-07-01 21:18:55 +0530

    IInquiryService added

---
+++ AutoHub.Application/Interfaces/IInquiryService.cs
@@ -0,0 +1,34 @@
+﻿using AutoHub.Application.DTOs.Inquiry;
+using AutoHub.Application.DTOs.Vehicles;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoHub.Application.Interfaces;
+
+public interface IInquiryService
+{
+    Task CreateInquiryAsync(
+        CreateInquiryRequest request,
+        Guid buyerId);
+
+    Task<PaginatedResponse<InquiryResponse>>
+        GetMyInquiriesAsync(
+            Guid buyerId,
+            InquirySearchRequest request);
+
+    Task<InquiryResponse>
+        GetInquiryByIdAsync(
+            Guid inquiryId,
+            Guid userId);
+
+    Task<PaginatedResponse<InquiryResponse>>
+        GetDealerInquiriesAsync(
+            Guid userId,
+            InquirySearchRequest request);
+
+    Task UpdateInquiryAsync(
+        Guid inquiryId,
+        UpdateInquiryRequest request,
+        Guid userId);
+}
