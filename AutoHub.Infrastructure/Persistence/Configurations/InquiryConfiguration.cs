a1d6511622ce3f44ef02d537badd72eb68398d5b
Jindal Mistry <mistryjindal@gmail.com>
2026-07-01 21:19:31 +0530

    InquiryConfiguration

---
+++ AutoHub.Infrastructure/Persistence/Configurations/InquiryConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using AutoHub.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AutoHub.Infrastructure.Persistence.Configurations;
+
+public class InquiryConfiguration : IEntityTypeConfiguration<Inquiry>
+{
+    public void Configure(EntityTypeBuilder<Inquiry> builder)
+    {
+        builder.HasIndex(x => x.VehicleId);
+
+        builder.HasIndex(x => x.BuyerId);
+
+        builder.HasIndex(x => x.DealerId);
+
+        builder.HasIndex(x => x.CreatedAt);
+
+        builder.HasIndex(x => x.Status);
+
+        builder.HasIndex(x => x.Type);
+    }
+}
