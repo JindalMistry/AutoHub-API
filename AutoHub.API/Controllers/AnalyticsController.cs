e350058849f5edc457ac41fff392cccb52500b99
Jindal Mistry <mistryjindal@gmail.com>
2026-07-03 21:21:41 +0530

    Analytics controller

---
+++ AutoHub.API/Controllers/AnalyticsController.cs
@@ -0,0 +1,32 @@
+﻿using AutoHub.Application.Common;
+using AutoHub.Application.DTOs.Vehicles;
+using AutoHub.Application.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AutoHub.API.Controllers;
+
+[ApiController]
+[Route("api/analytics")]
+public class AnalyticsController : ControllerBase
+{
+    private readonly IAnalyticsService _analyticsService;
+    public AnalyticsController(IAnalyticsService analyticsService)
+    {
+        _analyticsService = analyticsService;
+    }
+
+    [AllowAnonymous]
+    [HttpGet("trending")]
+    public async Task<IActionResult> GetTrendingVehiclesAsync()
+    {
+        var response = await _analyticsService.GetTrendingVehiclesAsync();
+
+        return Ok(new ApiResponse<List<VehicleListingResponse>>
+        {
+            Success = true,
+            Message = "Trending vehicles retrieved!",
+            Data = response
+        });
+    }    
+}
