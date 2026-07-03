3d0cc2d848f1c71aa516884966a9287d7e11aa58
Jindal Mistry <mistryjindal@gmail.com>
2026-07-03 21:21:05 +0530

    Analytics interface

---
+++ AutoHub.Application/Interfaces/IAnalyticsService.cs
@@ -0,0 +1,11 @@
+﻿using AutoHub.Application.DTOs.Vehicles;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoHub.Application.Interfaces;
+
+public interface IAnalyticsService
+{
+    Task<List<VehicleListingResponse>> GetTrendingVehiclesAsync();
+}
