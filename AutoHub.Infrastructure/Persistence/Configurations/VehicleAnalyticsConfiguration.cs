c24ec9c5f1b3330a938e4c311576fd94d32b9fd4
Jindal Mistry <mistryjindal@gmail.com>
2026-07-03 21:22:10 +0530

    Analytics Db relationships

---
+++ AutoHub.Infrastructure/Persistence/Configurations/VehicleAnalyticsConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using AutoHub.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AutoHub.Infrastructure.Persistence.Configurations;
+
+public class VehicleAnalyticsConfiguration : IEntityTypeConfiguration<VehicleAnalytics>
+{
+    public void Configure(EntityTypeBuilder<VehicleAnalytics> builder)
+    {
+        builder.HasKey(x => x.VehicleId);
+    }
+}
