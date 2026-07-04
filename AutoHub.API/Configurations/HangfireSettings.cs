e8b06ab322d145749e7e55c3abd8e73d45cf5769
Jindal Mistry <mistryjindal@gmail.com>
2026-07-04 21:23:08 +0530

    Hangfire settings added

---
+++ AutoHub.API/Configurations/HangfireSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace AutoHub.API.Configurations;
+
+public class HangfireSettings
+{
+    public string Username { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
