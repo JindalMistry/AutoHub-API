215b9b2d826e19a33421e625f933d13afeaa5089
Jindal Mistry <mistryjindal@gmail.com>
2026-07-04 21:24:14 +0530

    Vehicle tag configs

---
+++ AutoHub.Infrastructure/Persistence/Configurations/TagConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using AutoHub.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AutoHub.Infrastructure.Persistence.Configurations;
+
+public class TagConfiguration : IEntityTypeConfiguration<Tag>
+{
+    public void Configure(EntityTypeBuilder<Tag> builder)
+    {
+        builder.HasIndex(o => o.Name)
+            .IsUnique();
+    }
+}
---
+++ AutoHub.Infrastructure/Persistence/Configurations/VehicleTagConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using AutoHub.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AutoHub.Infrastructure.Persistence.Configurations;
+
+public class VehicleTagConfiguration : IEntityTypeConfiguration<VehicleTag>
+{
+    public void Configure(EntityTypeBuilder<VehicleTag> builder)
+    {
+        builder.HasKey(x => new
+        {
+            x.VehicleId,
+            x.TagId
+        });
+    }
+}
