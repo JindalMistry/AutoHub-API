02f0a896256f14271180850f08f5b57644f43d82
Jindal Mistry <mistryjindal@gmail.com>
2026-07-04 21:23:34 +0530

    background job service

---
+++ AutoHub.Application/Interfaces/IBackgroundJobService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoHub.Application.Interfaces;
+
+public interface IBackgroundJobService
+{
+    Task ExpireReservationsAsync();
+
+    Task RecalculateTrendingScoresAsync();
+
+    Task FlushAnalyticsCountersAsync();
+}
