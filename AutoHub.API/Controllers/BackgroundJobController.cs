cb268024a61d79373e932c45ed4ae1e53823a009
Jindal Mistry <mistryjindal@gmail.com>
2026-07-05 19:05:59 +0530

    Background Job controller

---
+++ AutoHub.API/Controllers/BackgroundJobController.cs
@@ -0,0 +1,29 @@
+﻿using AutoHub.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AutoHub.API.Controllers;
+
+[ApiController]
+[Route("api/jobs")]
+public class BackgroundJobController : ControllerBase
+{
+    private readonly IBackgroundJobService _backgroundJobService;
+    public BackgroundJobController(IBackgroundJobService backgroundJobService) 
+    {
+        _backgroundJobService = backgroundJobService;
+    }
+
+    [HttpPost("recalculate-trending")]
+    public async Task<IActionResult> RecalculateTrending()
+    {
+        await _backgroundJobService.RecalculateTrendingScoresAsync();
+        return Ok();
+    }
+
+    [HttpPost("expire-reservations")]
+    public async Task<IActionResult> ExpireReservations()
+    {
+        await _backgroundJobService.ExpireReservationsAsync();
+        return Ok();
+    }
+}
