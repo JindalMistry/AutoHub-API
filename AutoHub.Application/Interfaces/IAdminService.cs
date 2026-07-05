59c9bd2a26db4fbca2ca66f2ca4501447516cbb6
Jindal Mistry <mistryjindal@gmail.com>
2026-07-05 19:06:13 +0530

    IAdmin Service

---
+++ AutoHub.Application/Interfaces/IAdminService.cs
@@ -0,0 +1,19 @@
+﻿using AutoHub.Application.DTOs.Admin;
+using AutoHub.Application.DTOs.Vehicles;
+
+namespace AutoHub.Application.Interfaces;
+
+public interface IAdminService
+{
+    Task<AdminDashboardResponse> GetDashboardAsync();
+
+    Task<List<TopVehiclesResponse>> GetTopVehiclesAsync();
+
+    Task<List<TopDealerResponse>> GetTopDealersAsync();
+
+    Task<List<PendingDealerResponse>> GetPendingDealerAsync();
+
+    Task<List<CompletedReservationResponse>> GetActiveReservationsAsync();
+
+    Task<List<VehicleListingResponse>> GetPendingVehiclesAsync();
+}
