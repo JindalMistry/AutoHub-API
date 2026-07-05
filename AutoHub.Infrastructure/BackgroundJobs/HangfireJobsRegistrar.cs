add3da332390f886281664fd3df6cf9cdd6f778f
Jindal Mistry <mistryjindal@gmail.com>
2026-07-05 19:06:42 +0530

    Hangfire Registrar

---
+++ AutoHub.Infrastructure/BackgroundJobs/HangfireJobsRegistrar.cs
@@ -0,0 +1,31 @@
+﻿using AutoHub.Application.Interfaces;
+using Hangfire;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoHub.Infrastructure.BackgroundJobs;
+
+public static class HangfireJobRegistrar
+{
+    public static void Register()
+    {
+        RecurringJob.AddOrUpdate<
+            IBackgroundJobService>(
+            "expire-reservations",
+            x => x.ExpireReservationsAsync(),
+            "*/15 * * * *");
+
+        RecurringJob.AddOrUpdate<
+            IBackgroundJobService>(
+            "recalculate-trending",
+            x => x.RecalculateTrendingScoresAsync(),
+            "5 * * * *");
+
+        RecurringJob.AddOrUpdate<
+            IBackgroundJobService>(
+            "flush-analytics",
+            x => x.FlushAnalyticsCountersAsync(),
+            "0 * * * *");
+    }
+}
