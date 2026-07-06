e86e320693a20c69f303e6172be019ef16cdb706
Jindal Mistry <mistryjindal@gmail.com>
2026-07-06 21:51:47 +0530

    CacheController

---
+++ AutoHub.API/Controllers/CacheController.cs
@@ -0,0 +1,31 @@
+﻿using AutoHub.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AutoHub.API.Controllers;
+
+[ApiController]
+[Route("api/cache")]
+public class CacheController : ControllerBase
+{
+    private readonly ICacheService _cacheService;
+
+    public CacheController(ICacheService cacheService)
+    {
+        _cacheService = cacheService;
+    }
+
+    [HttpGet("cache-test")]
+    public async Task<IActionResult> Test()
+    {
+        await _cacheService.SetAsync(
+            "test-key",
+            "hello redis",
+            TimeSpan.FromMinutes(5));
+
+        var value =
+            await _cacheService
+                .GetAsync<string>("test-key");
+
+        return Ok(value);
+    }
+}
