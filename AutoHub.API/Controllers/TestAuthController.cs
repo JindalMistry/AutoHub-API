bdd4f608fa1ff444b3cfe2828834220f973edbf1
Jindal Mistry <mistryjindal@gmail.com>
2026-07-06 21:52:00 +0530

    Test controllers

---
+++ AutoHub.API/Controllers/TestAuthController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AutoHub.API.Controllers
+{
+    [ApiController]
+    [Route("api/auth-test")]
+    public class TestAuthController : ControllerBase
+    {
+        [HttpGet]
+        [Authorize]
+        public IActionResult Get()
+        {
+            return Ok("Authorized for all");
+        }
+
+        [HttpGet("admin")]
+        [Authorize(Roles = "Admin")]
+        public IActionResult AdminOnly()
+        {
+            return Ok("Admin Access");
+        }
+    }
+}
---
+++ AutoHub.API/Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using AutoHub.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/test")]
+public class TestController : ControllerBase
+{
+    private readonly IPasswordHasher _passwordHasher;
+
+    public TestController(IPasswordHasher passwordHasher)
+    {
+        _passwordHasher = passwordHasher;
+    }
+
+    [HttpGet]
+    public IActionResult Get()
+    {
+        var hash = _passwordHasher.Hash("Password123");
+
+        return Ok(hash);
+    }
+}
