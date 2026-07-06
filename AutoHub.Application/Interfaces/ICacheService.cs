abad4774abec51251bbf109920e3c8b3ac0cf06c
Jindal Mistry <mistryjindal@gmail.com>
2026-07-06 21:51:26 +0530

    ICacheService added

---
+++ AutoHub.Application/Interfaces/ICacheService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoHub.Application.Interfaces;
+
+public interface ICacheService
+{
+    Task<T?> GetAsync<T>(string key);
+
+    Task SetAsync<T>(
+        string key,
+        T value,
+        TimeSpan expiration);
+
+    Task RemoveAsync(string key);
+
+    Task<long> IncrementAsync(string key);
+
+    Task<long?> GetLongAsync(string key);
+
+    Task<long> DecrementAsync(string key);
+}
