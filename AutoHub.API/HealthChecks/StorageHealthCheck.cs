a3d791515061088651b3e4a6fc96c1f23044afed
Jindal Mistry <mistryjindal@gmail.com>
2026-07-29 16:40:35 +0530

    Health checks changed to work globally for all storages

---
+++ AutoHub.API/HealthChecks/StorageHealthCheck.cs
@@ -0,0 +1,43 @@
+﻿using AutoHub.Application.Interfaces;
+using AutoHub.Infrastructure.Configuration;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace AutoHub.API.HealthChecks
+{
+    public class StorageHealthCheck : IHealthCheck
+    {
+        private readonly IStorageService _storageService;
+        private readonly StorageSettings storageSettings;
+
+        public StorageHealthCheck(
+            IStorageService storageService, 
+            StorageSettings storageSettings)
+        {
+            _storageService = storageService;
+            this.storageSettings = storageSettings;
+        }
+
+        public async Task<HealthCheckResult> CheckHealthAsync(
+            HealthCheckContext context, 
+            CancellationToken cancellationToken = default)
+        {
+            var bucketName = storageSettings.BucketName;
+            var provider = storageSettings.Provider;
+
+            try
+            {
+                var exists = await _storageService.IsHealthyAsync(bucketName, cancellationToken);
+
+                return exists
+                    ? HealthCheckResult.Healthy($"{provider} is healthy.")
+                    : HealthCheckResult.Unhealthy("Bucket does not exist.");
+            }
+            catch (Exception ex)
+            {
+                return HealthCheckResult.Unhealthy(
+                    $"Unable to connect to {provider}.",
+                    ex);
+            }
+        }
+    }
+}
