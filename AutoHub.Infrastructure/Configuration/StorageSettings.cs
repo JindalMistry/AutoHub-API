04de367bae188dcf5f689226f8711ab37cdee3c9
Jindal Mistry <mistryjindal@gmail.com>
2026-07-29 15:55:54 +0530

    Added StorageSettings

---
+++ AutoHub.Infrastructure/Configuration/StorageSettings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoHub.Infrastructure.Configuration;
+
+public class StorageSettings
+{
+    public string Provider { get; set; } = string.Empty;
+
+    public string Endpoint { get; set; } = string.Empty;
+
+    public string BucketName { get; set; } = string.Empty;
+
+    public string AccessKey { get; set; } = string.Empty;
+
+    public string SecretKey { get; set; } = string.Empty;
+
+    public string Region { get; set; } = string.Empty;
+
+    public bool UseSSL { get; set; }
+}
